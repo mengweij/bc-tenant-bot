644886093b87171598546c223f62855e346d6208
mengwei <mengwei@example.com>
2025-01-02 17:49:56 -0800

    Installed and init shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,96 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        body: "#333333",
-        primary: "#123262",
-        secondary: "#4A5D79",
-        background: "#ffffff",
-        grey: "#F0F2F5",
-        lightGrey: "#F0F2F5",
-        border: "#DDE2E6",
-      },
-      boxShadow: {
-        custom: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			body: '#333333',
+  			primary: {
+  				DEFAULT: '#123262',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			grey: '#F0F2F5',
+  			lightGrey: '#F0F2F5',
+  			border: '#DDE2E6',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		boxShadow: {
+  			custom: '0 0 10px 0 rgba(0, 0, 0, 0.1)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		typography: {
+  			primary: {
+  				css: {
+  					'--tw-prose-body': 'hsl(var(--foreground))',
+  					'--tw-prose-headings': 'hsl(var(--primary))',
+  					'--tw-prose-lead': 'hsl(var(--foreground))',
+  					'--tw-prose-links': 'hsl(var(--primary))',
+  					'--tw-prose-bold': 'hsl(var(--primary))',
+  					'--tw-prose-counters': 'hsl(var(--primary))',
+  					'--tw-prose-bullets': 'hsl(var(--primary))',
+  					'--tw-prose-hr': 'hsl(var(--border))',
+  					'--tw-prose-quotes': 'hsl(var(--foreground))',
+  					'--tw-prose-quote-borders': 'hsl(var(--border))',
+  					'--tw-prose-captions': 'hsl(var(--muted-foreground))',
+  					'--tw-prose-code': 'hsl(var(--primary))',
+  					'--tw-prose-pre-code': 'hsl(var(--primary))',
+  					'--tw-prose-pre-bg': 'hsl(var(--muted))',
+  					'--tw-prose-th-borders': 'hsl(var(--border))',
+  					'--tw-prose-td-borders': 'hsl(var(--border))',
+  				},
+  			},
+  		},
+  	}
   },
   plugins: [
     require('@tailwindcss/typography'),
-  ],
+    require("tailwindcss-animate"),
+    
+],
 }
 
 export default config 
